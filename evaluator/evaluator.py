dde6863a30a175b5200c5f99e8141a04592d2a71
suragnair <suragnair@hotmail.com>
2019-02-26 00:25:31 -0800

    evaluator added

---
+++ evaluator/evaluator.py
@@ -0,0 +1,21 @@
+import os
+from utils.metrics import pearsonr_metric, spearmanr_metric
+
+
+class Evaluator():
+    def __init__(self, model, test_data_loader, config):
+        self.config = config
+        self.model = model
+        self.test_data_loader = test_data_loader
+
+    def evaluate(self):
+        preds, labels = self.model.predict_on_generator(self.test_data_loader)
+
+        taskwise_pearsonr, mean_pearsonr = pearsonr_metric(labels, preds)
+        taskwise_spearmanr, mean_spearmanr = spearmanr_metric(labels, preds)
+
+        print('Mean Pearson Correlation  : {:.4f}'.format(mean_pearsonr))
+        print('Mean Spearman Correlation : {:.4f}'.format(mean_spearmanr))
+
+        print('Taskwise Pearson Correlation  : {}'.format(taskwise_pearsonr))
+        print('Taskwise Spearman Correlation : {}'.format(taskwise_spearmanr))
